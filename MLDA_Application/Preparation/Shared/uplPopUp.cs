9a189cd756e32eb89a36f10a11d5495db9928643
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2023-03-01 19:58:50 +0530

    insert csv
    mongo driver added
    popup form
    save files
    insert in db

---
+++ MLDA_Application/Preparation/Shared/uplPopUp.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MLDA_Application.Shared;
+
+namespace MLDA_Application.Preparation.Shared
+{
+    public partial class uplPopUp : Form
+    {
+        public string filePath;
+        public uplPopUp(string fileName,string path)
+        {
+            InitializeComponent();
+            this.Text = String.Empty;
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+
+            txtFileName.Text = fileName;
+            filePath = path;
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.RootFolder=Environment.SpecialFolder.Desktop;
+            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFileLoc.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private bool saveFile()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV file (*.csv)|*.csv|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            saveFileDialog.InitialDirectory = txtFileLoc.Text;
+            saveFileDialog.FileName = txtFileName.Text;
+
+            string csvContent = File.ReadAllText(filePath);
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    sw.WriteLine(csvContent);
+                }
+            }
+
+            return true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(btnSave.Text=="Save")
+                {
+                bool save = saveFile();
+                if (save)
+                {
+                    MessageBox.Show("File Sucessfully saved", "Sucess");
+                }
+                else
+                {
+                    MessageBox.Show("File saving failed", "Error");
+                }
+                btnSave.Text = "Finish";
+
+            }else if(btnSave.Text=="Finish")
+            {
+                Dispose();
+            }
+           
+        }
+
+        private void btnCancl_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            string name = txtFileName.Text;
+            MongoCon mongoCon = new MongoCon();
+            bool upload = mongoCon.insertCsv(name,filePath);
+            if(upload)
+            {
+                MessageBox.Show("File Sucessfully to Database", "Sucess");
+                Dispose();
+            }else
+            {
+                MessageBox.Show("File Uploading Failed", "Error");
+            }
+        }
+    }
+}
--- MLDA_Application/Preparation/frmP_Import.cs
+++ MLDA_Application/Preparation/frmP_Import.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MLDA_Application.Preparation.Shared;
 
 namespace MLDA_Application.Preparation
 {
@@ -17,6 +18,7 @@ namespace MLDA_Application.Preparation
     {
         public string filePath;
         private UC_PC_missing ucM;
+        public string fileName;
         public frmP_Import()
         {
             InitializeComponent();
@@ -57,6 +59,7 @@ namespace MLDA_Application.Preparation
                     Console.WriteLine("imported: " + guna2TextBox1.Text);
                     filePath = openFileDialog1.FileName;
                     ucM.SetFilePath(filePath);
+                    fileName=Path.GetFileName(openFileDialog1.FileName);
                 }
                 else
                 {
@@ -89,7 +92,15 @@ namespace MLDA_Application.Preparation
                     int coloumIndex = 0;
                     foreach (string headerword in headerLabels)
                     {
-                        dr[headerword] = datawords[coloumIndex++];
+                        if(coloumIndex<datawords.Length)
+                        {
+                            dr[headerword] = datawords[coloumIndex++];
+                        }else
+                        {
+                            dr[headerword] = null;
+                            label1.Text = "This file has already Extracted"; 
+                                }
+                        
                     }
                     dt.Rows.Add(dr);
                 }
@@ -170,6 +181,13 @@ namespace MLDA_Application.Preparation
                 txtDsInfo.Text = output;
                 return filePath;
             }
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+           uplPopUp uplPopUp = new uplPopUp(fileName,filePath);
+            DialogResult result= uplPopUp.ShowDialog();
+
         }
     }
 }
