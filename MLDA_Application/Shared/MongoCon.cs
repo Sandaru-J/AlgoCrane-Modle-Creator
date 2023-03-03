4a68b28c67125dbb40906660af6675a6db174f49
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2023-03-03 20:14:33 +0530

    dataList method
    method for calling list of csv files in database

--- MLDA_Application/Shared/MongoCon.cs
+++ MLDA_Application/Shared/MongoCon.cs
@@ -61,5 +61,30 @@ namespace MLDA_Application.Shared
 
             return true;
         }
+        public List<string> dataList()
+        {
+            // Create a MongoDB client
+            var connectionString = "mongodb+srv://SiteUser:nAxKdh83uoIeaWFc@cluster0.h9ytvqs.mongodb.net";
+            var client = new MongoClient(connectionString);
+
+            // Get a database reference
+            var databaseName = "MLDA";
+            var database = client.GetDatabase(databaseName);
+
+            // Get a collection reference
+            var collectionName = "DataFiles";
+            var collection = database.GetCollection<BsonDocument>(collectionName);
+
+            var projection = Builders<BsonDocument>.Projection.Include("name");
+
+            // Retrieve the documents in the collection with only the names field
+            var documents = collection.Find(new BsonDocument()).Project(projection).ToList();
+
+            // Extract the names from the documents
+            var names = documents.Select(d => d["name"].AsString).ToList();
+            Console.WriteLine(names);
+            return names;
+        }
+
     }
 }
