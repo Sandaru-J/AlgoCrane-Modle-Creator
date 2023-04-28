94a78f03d1eb2690cdd43ddbafd7c9dd49a7d545
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2023-04-29 01:28:31 +0530

    Template main form added

---
+++ MLDA_Application/Template/FrmTemp_Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLDA_Application.Template
+{
+    public partial class FrmTemp_Main : Form
+    {
+        public FrmTemp_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
